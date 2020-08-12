b541356de582642e8cef80a0cb181704887af557
Dhanamjaya <dhanamjaya.k567@gmail.com>
2020-08-12 13:08:43 +0530

    remove ISynchronyService.java methods

--- src/main/java/com/app/service/ISynchronyService.java
+++ src/main/java/com/app/service/ISynchronyService.java
@@ -1,6 +1,5 @@
 package com.app.service;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,13 +8,5 @@ import com.app.model.Synchrony;
 @Service
 public interface ISynchronyService extends CrudRepository<Synchrony, Integer> {
 
-	@Query(" FROM Synchrony s WHERE s.syfId=:syfId")
-	Synchrony getMyData(Integer syfId);
-	
-	@Query("SELECT s.syfLoc FROM Synchrony s WHERE s.syfId=:syfId")
-	String getDetails(Integer syfId);
-	
-	@Query("SELECT s.syfId,s.syfLoc FROM Synchrony s WHERE s.syfId=:syfId")
-	Object getMultiDetails(Integer syfId);
 	
 	}
